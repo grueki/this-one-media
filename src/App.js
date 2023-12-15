c72d43deeccb738b77102a87e830d05bd421bbed
ijoh785 <isabelle.y.johns@gmail.com>
2023-12-16 11:04:04 +1300

    janky scroll

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import '@mantine/core/styles.css';
-import { AppShell, Stack } from '@mantine/core';
+import { AppShell, Stack, Text } from '@mantine/core';
 import React from "react"
 import { Jumbotron } from './components/Jumbotron/Jumbotron'
 import { Navbar } from './components/Navbar/Navbar'
@@ -8,20 +8,36 @@ import { ServicesBlock } from './components/Services/ServicesBlock'
 import { ContactForm } from './components/ContactForm/ContactForm'
 import { AboutUs } from './components/AboutUs/AboutUs'
 import { TheTeam } from './components/TheTeam/TheTeam'
+import { useScrollIntoView } from '@mantine/hooks';
 
 function App() {
+  const { scrollIntoView, targetRef } = useScrollIntoView<HTMLDivElement>({
+    offset: 60,
+  });
+
+  const scrollHome = () => {
+    // scrollHomeIntoView({ alignment: "start" })
+    console.log("fkdsfkdfdhsfkjdhfk")
+  }
+
   return (
     <AppShell header={{ height: 60 }}>
       <AppShell.Header>
-        <Navbar/>
+        <Navbar  
+          scrollHome={() =>
+            scrollIntoView({
+              alignment: 'center',
+            })} 
+        />
       </AppShell.Header>
       <AppShell.Main>
         <Stack>
           <Jumbotron/>
           <AboutUs/>
           <TheTeam/>
+          <Text ref={targetRef}>Hello there</Text>
           <ServicesBlock/>
-          <ContactForm/>
+          <ContactForm />
         </Stack>
       </AppShell.Main>
     </AppShell>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 import { Button, Group } from "@mantine/core"
 import classes from './Navbar.module.css'
 
-export const Navbar = () => {
+export const Navbar = ({ scrollHome }) => {
     return(
         <Group py={10} justify="space-between" className={classes.group}>
-            <Button color="rgba(255, 255, 255, 1)" variant="subtle">HOME</Button>
+            <Button color="rgba(255, 255, 255, 1)" variant="subtle" onClick={scrollHome}>HOME</Button>
             <Button color="rgba(255, 255, 255, 1)" variant="subtle">ABOUT US</Button>
             <Button color="rgba(255, 255, 255, 1)" variant="subtle">SERVICES</Button>
             <Button color="rgba(255, 255, 255, 1)" variant="subtle">CONTACT</Button>
