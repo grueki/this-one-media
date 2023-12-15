040b8d1c679641aadf991c4392482b0e0aaf1e12
ijoh785 <isabelle.y.johns@gmail.com>
2023-12-15 17:58:26 +1300

    form state captured

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,30 +1,43 @@
 import { Button, Divider, Group, Stack, Text, Textarea, TextInput, Title } from "@mantine/core"
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface IFormInput {
+    name: string;
+    email: string;
+    mobileNumber: string;
+    message: string;
+}
 
 export const ContactForm = () => {
+    const { register, handleSubmit } = useForm<IFormInput>()
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+    
     return(
-        <Stack align="center" bg={"#120e0b"} p={100}>
-            <Group justify="center">
-                <Stack mx={50} ta={"right"}>
-                    <Title c={"#ffffff"}>
-                        LIKE WHAT YOU SEE?
-                    </Title>
-                    <Text c={"#ffffff"}>
-                        Get in touch with us using the form opposite. We’ll get back to you as soon as we can. <br/>
-                        We look forward to working with you!
-                    </Text>
-                </Stack>
-                <Divider size="sm" orientation="vertical" />
-                <Stack mx={50}>
-                    <TextInput placeholder="Name" />
-                    <Group justify="space-between" grow>
-                        <TextInput placeholder="Email" />
-                        <TextInput placeholder="Mobile" />
-                    </Group>
-                    <Textarea placeholder="Message"/>
-                </Stack>
-            </Group>
-            <Button>Submit</Button>
-        </Stack>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack align="center" bg={"#120e0b"} p={100}>
+                <Group justify="center">
+                    <Stack mx={50} ta={"right"}>
+                        <Title c={"#ffffff"}>
+                            LIKE WHAT YOU SEE?
+                        </Title>
+                        <Text c={"#ffffff"}>
+                            Get in touch with us using the form opposite. We’ll get back to you as soon as we can. <br/>
+                            We look forward to working with you!
+                        </Text>
+                    </Stack>
+                    <Divider size="sm" orientation="vertical" />
+                    <Stack mx={50}>
+                        <TextInput placeholder="Email" {...register("email")}/>
+                        <Group justify="space-between" grow>
+                            <TextInput placeholder="Name" {...register("name")} />
+                            <TextInput placeholder="Mobile" {...register("mobileNumber")} />
+                        </Group>
+                        <Textarea placeholder="Message" {...register("message")}/>
+                    </Stack>
+                </Group>
+                <Button type="submit">Submit</Button>
+            </Stack>
+        </form>
         
         
     )
