96de8fd2e8d356d3dab6eb7bedcb043f0a60e53a
ijoh785 <isabelle.y.johns@gmail.com>
2023-12-20 12:46:41 +1300

    basic responsive sizing

--- src/components/TheTeam/TheTeam.tsx
+++ src/components/TheTeam/TheTeam.tsx
@@ -1,23 +1,25 @@
-import { BackgroundImage, Group, Stack, Text, Title } from "@mantine/core"
+import { BackgroundImage, Center, Group, Stack, Text, Title } from "@mantine/core"
 import classes from "./AboutUs.module.css"
 
 export const TheTeam = () => {
     return(
-        <Stack bg="#120e0b"  align="center" p={100}>
+        <Stack bg="#120e0b"  align="center" py={100}>
             <Title c="white" fz="3rem" ff={"'Josefin Sans', serif"}>THE TEAM</Title>
-            <Group w="50%" justify="space-between" py={50}>
-                <Stack w="400" p={50} align={"center"} bg={"white"}>
-                    <BackgroundImage className={classes.aboutUsImage} src={"https://raw.githubusercontent.com/grueki/this-one-media-images/main/img/david2.jpg"}/>
-                    <Title>David Fu</Title>
-                    <Text fs="italic">Co-director & Photographer </Text>
-                    <Text ta={"center"}>I work to create the magic of your brand that captures the audience's attention through still imagery.<br/><br/>I also drive everyone from A to B.</Text>
-                </Stack>
-                <Stack w="400" p={50} align={"center"} bg={"white"}>
-                    <BackgroundImage className={classes.aboutUsImage} src={"https://raw.githubusercontent.com/grueki/this-one-media-images/main/img/ari2.jpg"}/>
-                    <Title>Ari Boughen</Title>
-                    <Text fs="italic">Co-director & Videographer</Text>
-                    <Text ta={"center"}>I create stunning visuals through the lens of my camera and bring those stories to life in the editing suite.<br/><br/>Occasionally nocturnal.</Text>
-                </Stack>            
+            <Group justify="space-between">
+                <Center>
+                    <Stack className={classes.infoCard} w="400" p={50} align={"center"} bg={"white"}>
+                        <BackgroundImage className={classes.aboutUsImage} src={"https://raw.githubusercontent.com/grueki/this-one-media-images/main/img/david2.jpg"}/>
+                        <Title className={classes.infoCardTitle} ta={"center"}>David Fu</Title>
+                        <Text className={classes.infoCardText} ta={"center"} fs="italic">Co-director & Photographer </Text>
+                        <Text className={classes.infoCardText} ta={"center"}>I work to create the magic of your brand that captures the audience's attention through still imagery.<br/><br/>I also drive everyone from A to B.</Text>
+                    </Stack>
+                    <Stack className={classes.infoCard} w="400" p={50} align={"center"} bg={"white"}>
+                        <BackgroundImage className={classes.aboutUsImage} src={"https://raw.githubusercontent.com/grueki/this-one-media-images/main/img/ari2.jpg"}/>
+                        <Title className={classes.infoCardTitle} ta={"center"}>Ari Boughen</Title>
+                        <Text className={classes.infoCardText} ta={"center"} fs="italic">Co-director & Videographer</Text>
+                        <Text className={classes.infoCardText} ta={"center"}>I create stunning visuals through the lens of my camera and bring those stories to life in the editing suite.<br/><br/>Occasionally nocturnal.</Text>
+                    </Stack> 
+                </Center>           
             </Group>
         </Stack>
     )
